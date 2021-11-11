835591813ac6249c336dead15d2ed6047a2217f6
ElWazy <ElCazero.Fer@gmail.com>
2021-11-11 15:24:56 -0300

    Add tool Completed

---
+++ src/shared/infrastructure/RandomString.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LosYuntas\shared\infrastructure;
+
+final class RandomString
+{
+    public static function randomize($length = 8): string
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+}
