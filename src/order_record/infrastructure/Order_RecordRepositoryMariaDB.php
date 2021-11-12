b5b94ebe317e73cd6dd4be3ad8da934c155df176
Esteban Vivanco <esteban.vivanco.progra@gmail.com>
2021-11-12 00:51:44 -0300

    alo si
    alo si

--- src/order_record/infrastructure/Order_RecordRepositoryMariaDB.php
+++ src/order_record/infrastructure/Order_RecordRepositoryMariaDB.php
@@ -33,7 +33,7 @@ final class Order_RecordRepositoryMariaDB implements Order_recordRepository
                 order_record.order_date, 
                 order_record.deadline,
                 panolero.name AS panolero, 
-                
+
                 state.name AS estado
                 FROM order_record 
                 INNER JOIN user AS worker ON order_record.worker_id = worker.id 
@@ -47,7 +47,36 @@ final class Order_RecordRepositoryMariaDB implements Order_recordRepository
         return $statement->fetchAll(PDO::FETCH_ASSOC);
     }
 
+    public function getByCriteria(string $criteria): ?array
+    {   
 
+        // VARIAS BUSQUEDAS AL MISMO TIEMPO
+        $sql = 'SELECT 
+
+                worker.rut, 
+                worker.name AS trabajador, 
+                tool.name AS herramienta, 
+                order_record.amount, 
+                order_record.order_date, 
+                order_record.deadline,
+                panolero.name AS panolero, 
+
+                state.name AS estado
+
+                FROM order_record 
+                
+                INNER JOIN user AS worker ON order_record.worker_id = worker.id 
+                INNER JOIN user AS panolero ON order_record.panolero_id = panolero.id 
+                INNER JOIN tool ON order_record.tool_id = tool.id 
+                INNER JOIN state ON order_record.state_id = state.id';
+
+        $statement = $this->connection->prepare($sql);
+        $statement->execute([
+            'trabajador' => "%$criteria%"
+        ]);
+
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
 
  
 }
