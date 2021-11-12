35113c06a778da2806376edd1e5f74fd1b23331e
Gastón Hernán Cathalifaud Rosales <g.cathalifaud@alumnos.santotomas.cl>
2021-11-12 01:33:27 -0300

    Rut validation in user domain

--- app/views/tool/deactive.php
+++ app/views/tool/deactive.php
@@ -12,11 +12,13 @@
 <body>
     <nav>
         <ul class="menu_nav">
-            <li><a href="/">Inicio</a></li>
-            <li><a href="/user">Usuarios</a></li>
-            <li><a href="/category">Categorias</a></li>
-            <li><a href="/role">Roles</a></li>  
-        </ul>
+                <li><h1>Sistema Pañol</h1></li>
+                <li><a href="/">Inicio</a></li>
+                <li><a href="/user">Usuarios</a></li>
+                <li><a href="/category">Categorias</a></li>
+                <li><a href="/role">Roles</a></li>  
+                <li><a href="/logout">Logout</a></li>
+            </ul>
     </nav>
 
     <main >
