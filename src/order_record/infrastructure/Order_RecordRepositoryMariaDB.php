851f08bda9ee5c3a92b513be5eecfd49b64de053
Esteban Vivanco <esteban.vivanco.progra@gmail.com>
2021-11-11 23:53:05 -0300

    consulta select del GetAll order record
    akajkajkajakjakj

--- src/order_record/infrastructure/Order_RecordRepositoryMariaDB.php
+++ src/order_record/infrastructure/Order_RecordRepositoryMariaDB.php
@@ -24,7 +24,22 @@ final class Order_RecordRepositoryMariaDB implements Order_recordRepository
 
     public function getAll(): ?array
     {
-        $sql = '';
+        $sql = 'SELECT 
+
+                worker.rut, 
+                worker.name AS trabajador, 
+                tool.name AS herramienta, 
+                order_record.amount, 
+                order_record.order_date, 
+                order_record.deadline,
+                panolero.name AS panolero, 
+                
+                state.name AS estado
+                FROM order_record 
+                INNER JOIN user AS worker ON order_record.worker_id = worker.id 
+                INNER JOIN user AS panolero ON order_record.panolero_id = panolero.id 
+                INNER JOIN tool ON order_record.tool_id = tool.id 
+                INNER JOIN state ON order_record.state_id = state.id';
 
         $statement = $this->connection->query($sql);
  
